7a9b810ebb65dd6681ce2eef0a1ff331286736b1
Flavio Rocha <flavio.rocha@lambda3.com.br>
2022-06-22 16:23:44 -0300

    feat: adiciona icone para tab navigation

--- src/Routes/DetailsRoutes.jsx
+++ src/Routes/DetailsRoutes.jsx
@@ -1,20 +1,48 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native'
 import { Text, View } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import Details from '../Components/Details'
+
+const icon = ({
+  name, size, color, focused,
+}) => (
+  <Ionicons name={name} size={size} color={color} focused={focused}/>
+)
 
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Galeria!</Text>
     </View>
   )
 }
 const DetailsRoutes = () => {
   const Tab = createBottomTabNavigator()
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Descrição' component={Details} options={{ headerShown: false }}/>
-      <Tab.Screen name='Fotos' component={SettingsScreen} options={{ headerShown: false }}/>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#888',
+        display: getFocusedRouteNameFromRoute(route),
+      })}
+    >
+      <Tab.Screen
+        name='Sobre'
+        component={Details}
+        options={{
+          headerShown: false,
+          tabBarIcon: (props) => icon({ ...props, name: 'reader-outline' }),
+        }}
+      />
+      <Tab.Screen
+        name='Galeria'
+        component={SettingsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: (props) => icon({ ...props, name: 'images-outline' }),
+        }}
+      />
     </Tab.Navigator>
   )
 }
